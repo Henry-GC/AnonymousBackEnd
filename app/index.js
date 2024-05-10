2220e2bc52c8ecef0582aae44cedba7e54988a34
Henry-GC <henry.gon.cev@gmail.com>
2024-05-10 18:45:07 -0500

    try

--- app/index.js
+++ app/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const database = require("../database")
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 
 // Permitir solicitudes Cors
-allowedOrigin = ["https://anonymouspc.netlify.app","http://localhost:3000"]
+allowedOrigin = ["https://anonymouspc.netlify.app","http://localhost:3000","https://anonymousbackend.onrender.com"]
 app.use(cors({
   origin: allowedOrigin,
 }));
