6df8c46ae7594cbd92a52dde4a2f153f86c4a6f5
Henry-GC <henry.gon.cev@gmail.com>
2024-07-31 15:28:20 -0500

    remove port of bd

--- utils/config.js
+++ utils/config.js
@@ -7,7 +7,6 @@ export const CORS_URL = process.env.CORS
 export const SECRET_KEY = process.env.SECRET_KEY
 
 export const DB_HOST = process.env.POSTGRES_HOST
-export const DB_PORT = process.env.POSTGRES_PORT
 export const DB_USER = process.env.POSTGRES_USER
 export const DB_PASS = process.env.POSTGRES_PASSWORD
 export const DB_NAME = process.env.POSTGRES_DATABASE
--- utils/database.js
+++ utils/database.js
@@ -4,7 +4,6 @@ import { DB_HOST, DB_USER, DB_PASS, DB_NAME } from "./config.js"
 
 export const pool = new pg.Pool({
     host: DB_HOST,
-    port: 5432,
     user: DB_USER,
     password: DB_PASS,
     database: DB_NAME,
