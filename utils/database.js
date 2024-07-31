241a65b2498ee6016fb27c8c90d45453131724ee
Henry-GC <henry.gon.cev@gmail.com>
2024-07-31 15:13:39 -0500

    change port db

--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,10 @@
 import pg from "pg"
-import { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } from "./config.js"
+import { DB_HOST, DB_USER, DB_PASS, DB_NAME } from "./config.js"
 
 
 export const pool = new pg.Pool({
     host: DB_HOST,
-    port: DB_PORT,
+    port: 5432,
     user: DB_USER,
     password: DB_PASS,
     database: DB_NAME,
