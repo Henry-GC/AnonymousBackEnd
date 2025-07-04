bb538434c30e87b5661b4012a0f641de77ea4497
Henry-GC <henry.gon.cev@gmail.com>
2025-07-04 14:43:29 -0500

    change rutas

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -6,8 +6,8 @@ import { verify } from "../controllers/verify.js"
 const router = express.Router()
 
 router.post('/create', admin.create)
-router.post('/login', admin.login)
-router.get('/logout', verify.verifyTokenAdmin, admin.logout)
+router.post('/admLogin', admin.login)
+router.get('/admLogout', verify.verifyTokenAdmin, admin.logout)
 router.get('/products', products.productsData)
 router.post('/products/create', products.createProduct)
 
