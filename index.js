e4329a514d8dcd98999324f5688963abbdb8d987
Henry-GC <henry.gon.cev@gmail.com>
2025-07-04 13:25:50 -0500

    clear comments

--- index.js
+++ index.js
@@ -16,23 +16,7 @@ const allowedOrigins = [
   // "http://localhost:3000"
 ];
 
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     if (allowedOrigins.includes(origin) || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Bloqueado por CORS'));
-//     }
-//   },
-//   credentials: true,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// };
-
-// app.use(cors(corsOptions));
-
 app.use(cors({origin: allowedOrigins, credentials: true}));
-// app.options("*", cors({origin: allowedOrigins, credentials: true}));
 app.use(express.json());
 app.use(cookieParser())
 
